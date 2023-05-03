164437ba9d7ccb900824fe71ffab1817d069740e
vats0914 <72242500+vats0914@users.noreply.github.com>
2023-05-03 08:22:16 +0530

    Created movies and Review classes

---
+++ src/main/java/dev/vatsal/movies/movies.java
@@ -0,0 +1,32 @@
+package dev.vatsal.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class movies {
+    @Id
+    private ObjectId id ;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backDrops;
+    @DocumentReference
+    private List<String> reviewIds;
+
+
+
+}
