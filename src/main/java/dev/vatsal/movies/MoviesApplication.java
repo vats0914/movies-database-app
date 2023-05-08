f347c605dea3545adc04b9fb016697a8cc4311f0
vats0914 <72242500+vats0914@users.noreply.github.com>
2023-05-08 19:45:11 +0530

    Testing some changes.

--- src/main/java/dev/vatsal/movies/MoviesApplication.java
+++ src/main/java/dev/vatsal/movies/MoviesApplication.java
@@ -1,5 +1,6 @@
 package dev.vatsal.movies;
-
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,14 @@ public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+
 	}
 
 	@GetMapping("/root")
 	public String apiRoot(){
 		return "Hello World!" ;
 	}
+
+
 
 }
